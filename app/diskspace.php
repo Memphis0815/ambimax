063f7b61a649bf5f4c376043875f4dffec9f3ca5
Memphis0815 <35699585+Memphis0815@users.noreply.github.com>
2021-05-18 12:31:32 +0200

    converter finished

--- app/diskspace.php
+++ app/diskspace.php
@@ -3,21 +3,27 @@ declare(strict_types = 1);
 define('MAX_VALUE', 9223372036854775807);
 
 //überprüft ob eine gegebene Zahl >0 und < 9223372036854775807 ist
-function checkNumber(int $number): bool {
+function checkNumber($number): bool
+{
     if($number > 0 && $number <= MAX_VALUE) {
         return true;
     }
     return false;
 }
-function convertGBtoMB(float $number): float {
-    return $number * 1000;
+function convertGBtoMB($number): float {
+    return $number * 1024;
 }
 
 function picturecount() {
-    $disksize = $_POST['harddisksize'];
-    $picturesize = $_POST['picturesize'];
-    if(checkNumber($disksize) && checkNumber($picturesize)) {
-        return floor(fdiv(concertGBtoMB($disksize), $picturesize));
+    $disk = (float)$_POST['harddisksize'];
+    //echo $disk;
+    $picture = (float)$_POST['picturesize'];
+    if(checkNumber(convertGBtoMB($disk)) && checkNumber($picture)) {
+       /** $disk = convertGBtoMB($disk);
+        $count = fdiv($disk, $picture);
+        $count = floor($count);
+        return $count; **/
+        return floor(fdiv(convertGBtoMB($disk), $picture));
     }
 }
 ?>
@@ -25,12 +31,14 @@ function picturecount() {
 <form>
 
     <p>Bildgröße in Mb dezimal :
-        <input type="number" name="picturesize">
+        <input type="text" name="picturesize">
     </p>
-    <p>max zufällige Primzahlen:
-        <input type="number" name="harddisksize">
-        <input type="submit" value="Primzahlen max" formaction="diskspace.php" formmethod="post">
-        Es können <?php picturecount() ?> auf der Festplatte gespeichert werden.
+    <p>Festplattengröße in Gb:
+        <input type="text" name="harddisksize">
+        <input type="submit" value="OK" formaction="diskspace.php" formmethod="post"> </p>
+        <?php echo 'Die aktuelle PHP Version ist ' . phpversion(); ?>
+    <p>
+        Es können <?php echo picturecount() ?> auf der Festplatte gespeichert werden.
     </p>
 
 </form>
